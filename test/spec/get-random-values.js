bd21042645886c18ea02b80c3c6ad577763993e1
Geoff Cox <redgeoff@gmail.com>
2015-11-29 18:08:28 +0000

    fix(bcrypt): polyfill get random values

--- test/spec/get-random-values.js
+++ test/spec/get-random-values.js
@@ -5,7 +5,9 @@ var getRandomValues = require('../../scripts/get-random-values');
 describe('get-random-values', function () {
 
   it('should get random values', function () {
-    var array = new Uint8Array(10);
+    // Uint8Array is not supported in IE 9
+    // var array = new Uint8Array(10);
+    var array = [0, 0, 0];
     getRandomValues(array);
 
     for (var i = 0; i < array.length; i++) {
